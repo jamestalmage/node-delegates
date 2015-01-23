cbc2acec17ea4ea39eb3d8564fae001e275709e6
James Talmage <james.talmage@jrtechnical.com>
2015-01-22 21:52:36 -0500

    Make performance test more complicated.

--- perf.js
+++ perf.js
@@ -10,6 +10,18 @@ function createDelegator(v){
   v(Constructor.prototype,'delegate')
     .method('testMethod')
     .access('testProperty')
+    .access('a')
+    .access('b')
+    .access('c')
+    .access('d')
+    .access('e')
+    .access('f')
+    .access('g')
+    .access('h')
+    .access('i')
+    .access('j')
+    .access('k')
+    .access('l')
     .fluent('fluentProp');
 
   return Constructor;
@@ -23,10 +35,26 @@ Delegate.prototype.testMethod = function(){
   return this.testProperty;
 };
 
-function runTest(delegator){
+function runTest(Constructor){
   var start = Date.now();
   for(var value = 0; value < 1000000; value++){
+    var delegator = new Constructor(new Delegate());
     delegator.testProperty = value;
+    delegator.a = delegator.b = delegator.c = delegator.d = value;
+    delegator.e = delegator.f = delegator.g = delegator.h = value;
+    delegator.i = delegator.j = delegator.k = delegator.l = value;
+    delegator.a.should.equal(value);
+    delegator.b.should.equal(value);
+    delegator.c.should.equal(value);
+    delegator.d.should.equal(value);
+    delegator.e.should.equal(value);
+    delegator.f.should.equal(value);
+    delegator.g.should.equal(value);
+    delegator.h.should.equal(value);
+    delegator.i.should.equal(value);
+    delegator.j.should.equal(value);
+    delegator.k.should.equal(value);
+    delegator.l.should.equal(value);
     delegator.testProperty.should.equal(value);
     delegator.testMethod().should.equal(value);
     delegator.fluentProp(value);
@@ -39,10 +67,7 @@ function runTest(delegator){
   return time;
 }
 
-var d1 = new D1(new Delegate());
-var d2 = new D2(new Delegate());
-
-var t2 = runTest(d2);
-var t1 = runTest(d1);
+var t2 = runTest(D2);
+var t1 = runTest(D1);
 
 console.log('new version ' + (Math.round(((t1-t2)/t2)*10000)/100) + '% faster');
